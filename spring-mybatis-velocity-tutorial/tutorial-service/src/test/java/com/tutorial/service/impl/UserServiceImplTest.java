0b65974073803b90bcb3eeb255d60c84d859315e
rolyer <irolyer@gmail.com>
2014-10-16 10:38:00 +0800

    Add EasyMock for Service

--- spring-mybatis-velocity-tutorial/tutorial-service/src/test/java/com/tutorial/service/impl/UserServiceImplTest.java
+++ spring-mybatis-velocity-tutorial/tutorial-service/src/test/java/com/tutorial/service/impl/UserServiceImplTest.java
@@ -2,12 +2,16 @@ package com.tutorial.service.impl;
 
 import java.util.Date;
 
+import org.easymock.EasyMock;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
+import com.tutorial.dao.UserDao;
 import com.tutorial.model.User;
 import com.tutorial.service.UserService;
 
@@ -18,9 +22,23 @@ public class UserServiceImplTest {
 	@Autowired
 	private UserService userService;
 	
+	private UserDao userDao;
+	
+	@Before
+	public void before() {
+		userDao = EasyMock.createMock(UserDao.class);
+		
+		ReflectionTestUtils.setField(userService, "userDao", userDao);
+	}
+	
 	@Test
 	public void testAdd() {
+		EasyMock.expect(userDao.insert(EasyMock.anyObject(User.class))).andReturn(1);
+		EasyMock.replay(userDao);
+		
 		User user =  new User("account", "password", "email", "tel", "im", 1, new Date(), new Date());
 		userService.add(user);
+		
+		EasyMock.verify(userDao);
 	}
 }
